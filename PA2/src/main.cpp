0df8c9732f0a6d48162941324b7c17e698f22016
Guanymsl <chadlee9402@gmail.com>
2024-11-02 20:07:12 +0800

    Add Run Time

--- PA2/src/main.cpp
+++ PA2/src/main.cpp
@@ -1,8 +1,12 @@
+#include "../lib/tm_usage.h"
 #include <iostream>
 #include <fstream>
 #include "max_planar_subset.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]){
+    CommonNs::TmUsage tmusg;
+    CommonNs::TmStat stat;
+
     fstream fin(argv[1]);
     fstream fout;
     fout.open(argv[2], ios::out);
@@ -11,19 +15,25 @@ int main(int argc, char* argv[]) {
     fin >> n;
     int i, j;
     int* vertex = new int[n];
-    for (int k = 0; k < n / 2; k++) {
+    for(int k = 0; k < n / 2; k++){
         fin >> i >> j;
         vertex[i] = j;
         vertex[j] = i;
     }
 
+    tmusg.periodStart();
+
     vector<pii> ans = mps(n, vertex);
 
     fout << ans.size() << '\n';
-    for (int i = 0; i < ans.size(); i++)
+    for(int i = 0; i < ans.size(); i++)
         fout << ans[i].ff << " " << ans[i].ss << '\n';
 
     delete[] vertex;
+
+    tmusg.getPeriodUsage(stat);
+    cout << "The total CPU time: " << (stat.uTime + stat.sTime) / 1000.0 << "ms\n";
+    cout << "memory: " << stat.vmPeak << "KB\n";
 
     fin.close();
     fout.close();
